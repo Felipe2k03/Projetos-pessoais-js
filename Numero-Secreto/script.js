37837e9e2aafb2a9bf0de4ab9ab8f90ef1ff02e1
Felipe2k03 <luisfelipe2k03@gmail.com>
2025-06-25 22:50:46 -0300

    fiz algumas modificações nos simbolos do html, implementei parcialmente o javascript, falta algumas funcionalidades porem vou deixar para fazer na proxima oportunidade.

---
+++ Numero-Secreto/script.js
@@ -0,0 +1,88 @@
+const result = document.querySelector(".result");
+const buttons = document.querySelectorAll(".buttons button");
+
+let currentNumber = "";
+let firstOperand = null;
+let operator = null;
+let restart = false;
+
+// funtions
+
+function updateResult(originClear = false) {
+  result.innerText = originClear ? 0 : currentNumber.replace(".", ",");
+}
+
+//concatenando os numeros digitados
+function addDigit(digit) {
+  if (digit === "," && (currentNumber.includes(",") || !currentNumber)) return;
+
+  if (restart) {
+    currentNumber = digit;
+    restart = false;
+  } else {
+    currentNumber += digit;
+  }
+
+  updateResult();
+}
+
+function setOperator(newOperator) {
+  if (currentNumber) {
+    calculate();
+    firstOperand = parseFloat(currentNumber.replace(",", "."));
+    currentNumber = "";
+  }
+
+  operator = newOperator;
+}
+
+function calculate() {
+  if (operator === null || firstOperand === null) return;
+  let secondOperand = parseFloat(currentNumber.replace(",", "."));
+  let resultValue;
+
+  switch (operator) {
+    case "+":
+      resultValue = firstOperand + secondOperand;
+      break;
+    case "-":
+      resultValue = firstOperand - secondOperand;
+      break;
+    case "x":
+      resultValue = firstOperand * secondOperand;
+    case "÷":
+      resultValue = firstOperand / secondOperand;
+      break;
+    default:
+      return;
+  }
+
+  if (resultValue.toString().split(".")[1]?.length > 5) {
+    currentNumber = parseFloat(resultValue.toFixerd(5)).toString();
+  } else {
+    currentNumber = resultValue.toString();
+  }
+
+  operator = null;
+  firstOperand = null;
+  restart = true;
+  percentageValue = null;
+  updateResult();
+}
+
+// buttons
+
+//adicionando numero no "Visor"
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const buttonText = button.innerText;
+    if (/^[0-9,]+$/.test(buttonText)) {
+      addDigit(buttonText);
+      //verificar se o click é um operador
+    } else if (["+", "-", "x", "÷"].includes(buttonText)) {
+      setOperator(buttonText);
+    } else if (buttonText === "=") {
+      calculate();
+    }
+  });
+});
