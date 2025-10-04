d791b4c3d10ce73cc382b2214c075aa7c76d9b36
Felipe2k03 <luisfelipe2k03@gmail.com>
2025-10-04 00:15:07 -0300

    feat: adding words for strength

---
+++ Forca/assets/js/words.js
@@ -0,0 +1,12 @@
+const words = [
+  { word: "Abacaxi", clue: "Fruta tropical" },
+  { word: "Banana", clue: "Fruta favorita do macaco" },
+  { word: "Morango", clue: "Fruta Vermelha" },
+  { word: "Abacate", clue: "Usado na Guacamole" },
+  { word: "Tamarindo", clue: "Usado no Suco do chaves" },
+  { word: "Cachorro", clue: "Melhor amigo do Homem" },
+  { word: "Calopsita", clue: "passaro que assobia" },
+  { word: "Gato", clue: " animal peludo de belos olhos" },
+  { word: "Elefante", clue: "Animal cinza de longa tromba" },
+  { word: "GOlfinho", clue: "Animal marinho super inteligente" },
+];
