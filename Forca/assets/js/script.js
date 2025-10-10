6e6495fbb08eeb18e0a4d33a12725621b7d7427a
Felipe2k03 <luisfelipe2k03@gmail.com>
2025-10-09 23:58:24 -0300

    feat: add a function 'generateGuessSection'

--- Forca/assets/js/script.js
+++ Forca/assets/js/script.js
@@ -1,10 +1,10 @@
 import getWord from "./words.js";
 
 const contentBtns = document.querySelector(".btns");
-const contentGuessWord = document.querySelector(".guess-word");
+const contentGuessWord = document.querySelector(".guessWord");
 const img = document.querySelector("img");
 const contentClue = document.querySelector(".clue");
-const btnNew = document.querySelector(".new");
+const btnNew = document.querySelector(".newGame");
 let indexImg;
 
 init();
@@ -13,7 +13,25 @@ function init() {
   indexImg = 1;
   img.src = "assets/img/img1.png";
 
+  generateGuessSection();
   generateButtons();
+}
+
+function generateGuessSection() {
+  contentGuessWord.textContent = "";
+
+  const { word, clue } = getWord();
+
+  // mostra a dica
+  contentClue.textContent = `Dica: ${clue}`;
+
+  Array.from(word).forEach((letter) => {
+    const span = document.createElement("span");
+
+    span.textContent = "_";
+    span.setAttribute("word", letter.toUpperCase());
+    contentGuessWord.appendChild(span);
+  });
 }
 
 function generateButtons() {
@@ -23,6 +41,11 @@ function generateButtons() {
     const btn = document.createElement("button");
     const letter = String.fromCharCode(i).toUpperCase(); //fromcharcode possui o alfabeto da posição 97 ate o 122
     btn.textContent = letter; //bota as letras dentro do botão
+
+    btn.onclick = () => {
+      btn.disabled = true;
+      btn.style.backgroundColor = "gray";
+    };
 
     contentBtns.appendChild(btn);
   }
